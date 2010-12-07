347b03ca0cda6e53a4bedfe8cdff3268e3b97e4e
John Napiorkowski <jjn1056@yahoo.com>
2010-12-07 16:35:49 -0500

    first commit

---
+++ lib/Benchmark/Catalyst.pm
@@ -0,0 +1,27 @@
+package Catalyst::Benchmark;
+ 
+use 5.008008;
+our $VERSION = '0.01';
+ 
+=head1 NAME
+
+Catalyst::Benchmark - Tools to bench various Catalyst servers
+
+=head1 DESCRIPTION
+
+A sample application and some tools for us to have the ability to benchmark 
+new backends (such as PSGI/Plack)
+
+=head1 AUTHOR
+
+John Napiorkowski, C<< <jjnapiork@cpan.org> >>
+
+=head1 COPYRIGHT & LICENSE
+
+This program is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
+
