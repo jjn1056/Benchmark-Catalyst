6f8e67e651fd0d420f81b7b69674dc90e50c44ba
John Napiorkowski <jjn1056@yahoo.com>
2010-12-07 18:23:34 -0500

    some results to start with

--- benchmarks/basic.pl
+++ benchmarks/basic.pl
@@ -16,12 +16,15 @@ timethis
 
 __END__
 
+## against psgi branch Dec 7,2010
 $ perl -I ~/Desktop/Catalyst/5.80/branches/psgi/lib/ -I lib benchmarks/basic.pl 
 timethis 5000:  9 wallclock secs ( 9.39 usr +  0.01 sys =  9.40 CPU) @ 531.91/s (n=5000)
 
-perl -I ~/Desktop/Catalyst/5.80/trunk/lib/ -I lib benchmarks/basic.pl 
-timethis 5000: 19 wallclock secs (13.99 usr +  1.85 sys = 15.84 CPU) @ 315.66/s (n=5000)
+## against trunk on Dec 7, 2010
+$ perl -I ~/Desktop/Catalyst/5.80/trunk/lib/ -I lib benchmarks/basic.pl 
+timethis 5000: 20 wallclock secs (14.10 usr +  1.96 sys = 16.06 CPU) @ 311.33/s (n=5000)
 
+## Baseline, from CPAN
 $ perl -I lib benchmarks/basic.pl
 timethis 5000: 14 wallclock secs (11.86 usr +  1.58 sys = 13.44 CPU) @ 372.02/s (n=5000)
 
